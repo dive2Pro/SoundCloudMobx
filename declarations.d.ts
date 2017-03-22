edca8d7194de60793a51fb39bb53a1c954d7ce9d
chuancey <toylucklebo@gmail.com>
2017-03-22 15:30:07 +0800

    fix(react-dom):remove @types

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,9 +1,8 @@
-
-declare module 'react-hot-loader';
+// declare module 'react-hot-loader';
 declare module 'soundcloud';
 declare module 'js-cookie'
 declare module 'mobx-remotedev';
 declare module 'react-router-dom';
-
+declare module 'react-dom';
 
 declare module '*.scss';
